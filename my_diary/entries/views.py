0882b4d09585a27360d736510c5ede6985f5b616
ihamzak <ham-xa@hotmail.com>
2021-01-05 05:32:35 +0000

    added model

--- my_diary/entries/views.py
+++ my_diary/entries/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Entry
+from .forms import EntryForm
 
 def index(request):
-    return render(request,'entries/index.html')
+    entries = Entry.objects.order_by('-date_posted')
+    context = {'entries':entries}
+    return render(request,'entries/index.html',context)
 
 def add(request):
-    return render(request,'entries/add.html')
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+
+        forms = EntryForm()
+        
+    form = EntryForm()
+    context = {'form':form}
+    return render(request,'entries/add.html',context)
